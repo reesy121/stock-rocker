fb9eabbd2bf6619a20fe8793ae514981aa1555b1
Dave Rees <reesy121@hotmail.co.uk>
2016-05-24 22:20:49 +0100

    Fix Cap error

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,14 +50,14 @@ namespace :deploy do
   after :publishing, :restart
   before  'deploy:assets:precompile', 'deploy:migrate'
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-         execute :rake, 'cache:clear'
-       end
-    end
-  end
+ # after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
+   #   # Here we can do anything such as:
+   #   within release_path do
+   #      execute :rake, 'cache:clear'
+   #    end
+   # end
+  #end
 
   desc "Check that we can access everything"
   task :check_write_permissions do
