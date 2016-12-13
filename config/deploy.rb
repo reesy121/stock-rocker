82b21357216cdd3aba9d13581663c4a0758beea5
Dave Rees <reesy121@hotmail.co.uk>
2016-12-13 21:09:49 +0000

    fixed routes file

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,13 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{fetch(:stage)}"
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
   end
 
   after :publishing, :restart
