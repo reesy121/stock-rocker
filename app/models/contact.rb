c76ec01aaab69ad8426c6b63a532af8ac9a2f29d
Dave Rees <reesy121@hotmail.co.uk>
2017-01-23 21:41:59 +0000

    comment controller tests

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
-class Contact #< ActiveRecord::Base #Only for sending emails
+class Contact #< ActiveRecord::Base 
+  #Only for sending emails
 
   include ActiveModel::Model
   include ActiveModel::Conversion
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
 
-   describe "GET #new" do
-    it "returns http success" do
+    it "get returns http success" do
       get :new
+      
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "POST #create" do
     it "returns http success" do
       post :create, {:contact => contact_attributes}
+
       expect(response).to have_http_status(:success)
     end
-  end
 
 end
